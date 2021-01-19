a51049dcfb66f847e673c1c96d9ff4222be4d4d0
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-01-19 17:59:59 +0900

    feat : listener

---
+++ src/listener.cpp
@@ -0,0 +1,34 @@
+//
+// Created by yn4k4nishi on 2021/01/19.
+//
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+using std::placeholders::_1;
+
+class MinimalSubscriber : public rclcpp::Node
+{
+public:
+    MinimalSubscriber()
+            : Node("listener")
+    {
+        subscription_ = this->create_subscription<std_msgs::msg::String>(
+                "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    }
+
+private:
+    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+    {
+        RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+    }
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<MinimalSubscriber>());
+    rclcpp::shutdown();
+    return 0;
+}
